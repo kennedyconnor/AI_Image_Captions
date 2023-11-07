d93bf6add0678772f452fc2151406b26abebcab8
Connor Kennedy <kennedyconnor02@gmail.com>
2023-11-07 01:59:43 -0500

    minor adjustments to text html

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,6 +5,8 @@ function HomePage() {
   return (
     <div name="Home">
       <h1>AI Image Captioning by Connor Kennedy</h1>
+      <h2>Welcome. Upload an Image File and receive a caption!</h2>
+      <h3>Note: AI generation is WIP. You will receive a prewritten caption. Come back in the future!</h3>
       <ImageUploadForm />
     </div>
   );
