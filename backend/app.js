a598fa847eeb9a9ab7e49a207dbd9dc3e3c5e570
kennedyconnor <kennedyconnor02@gmail.com>
2023-11-20 14:20:51 +0100

    visionService first pass + backend organizing

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const captionRoutes = require('./routes/captionRoutes');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use('/api', captionRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- backend/src/services/visionService.js
+++ backend/src/services/visionService.js
@@ -1,5 +1,23 @@
-// This is a placeholder for the Google Cloud Vision API logic
+const vision = require('@google-cloud/vision');
+
+// Creates a client using credentials from a JSON key file
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: 'path/to/your/google-credentials.json'
+});
+
 exports.generateCaption = async (image) => {
-    // Mock response for now
-    return "This is a mock caption for the image.";
-  };
+  try {
+    // Assuming 'image' is a base64-encoded image string
+    const [result] = await client.labelDetection({ image: { content: image } });
+    const labels = result.labelAnnotations;
+
+    // Extract descriptions from the labels
+    const captions = labels.map(label => label.description);
+
+    // Join the descriptions into a single string
+    return captions.join(', ');
+  } catch (error) {
+    console.error('Error in Vision API:', error);
+    throw error;
+  }
+};
