e8afe52cafcfbdb9121a9b9af31521aad720b4de
kennedyconnor <kennedyconnor02@gmail.com>
2023-12-11 22:20:01 -0500

    styling changes

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -5,8 +5,8 @@ function HomePage() {
   return (
     <div name="Home">
       <h1>AI Image Captioning by Connor Kennedy</h1>
-      <h2>Welcome. Upload an Image File and receive a caption!</h2>
-      <h3>Don't worry! Styling is on its way.</h3>
+      <h2>Welcome. Upload an Image File and receive a caption.</h2>
+      <h3>Thanks for visiting!</h3>
       <ImageUploadForm />
     </div>
   );
